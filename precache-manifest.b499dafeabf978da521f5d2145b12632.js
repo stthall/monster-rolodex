d077f16a4ec6d0868e17a7f02597ba4fb01cde9b
stu <stthall@icloud.com>
2020-07-06 20:17:07 -0700

    Updates

---
+++ precache-manifest.b499dafeabf978da521f5d2145b12632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947538595732f32e650ead5f861655f1",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "69587abc48675d46b6f0",
+    "url": "/monster-rolodex/static/css/main.5e3514eb.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69587abc48675d46b6f0",
+    "url": "/monster-rolodex/static/js/main.ef861186.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
